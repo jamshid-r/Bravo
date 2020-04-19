69fa03778575dce9f18a6383db3ee0ccbb7ee6ee
mkits <mkits@mindgrub.com>
2020-04-19 02:28:39 -0400

    Hotel POM

--- src/test/java/com/orbitz/pages/HotelPage.java
+++ src/test/java/com/orbitz/pages/HotelPage.java
@@ -1,5 +1,26 @@
 package com.orbitz.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.orbitz.utils.Driver;
+
 public class HotelPage {
+
+	public HotelPage() {
+		PageFactory.initElements(Driver.getDriver(), this);
+	}
+
+	@FindBy(id = "tab-hotel-tab-hp")
+	public WebElement linkHotel;
+
+	@FindBy(id = "hotel-destination-hp-hotel")
+	public WebElement goingToInputField;
+
+	@FindBy(id = "hotel-flight-origin-hp-hotel")
+	public WebElement flyingFromInputField;
+
+	@FindBy(id = "//span[.='Search Hotel + Flight']")
+	public WebElement searchHotelFligthButton;
 
 }
